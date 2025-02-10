986ee4d967ebb919966d1a9139fcf47cb1eb39b5
GeirOwe <geir.owe@hotmail.com>
2025-02-10 19:58:35 +0100

    host at render

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# this app is deployed to Render
+# url: https://golf-app-w497.onrender.com
+# dashboard on Render: https://dashboard.render.com/web/srv-cul4m20gph6c738frqj0
+# OBS: Your free instance will spin down with inactivity, which can delay requests by 50 seconds or more.
+
 from flask import Flask, render_template, request, redirect, url_for
 from models import Player
 
@@ -18,3 +23,7 @@ def add_player():
         player = Player(player_name, float(handicap))
         players.append(player)
     return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    # This is used when running locally
+    app.run(host='0.0.0.0', port=10000)
